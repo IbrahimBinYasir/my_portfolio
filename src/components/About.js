ead421eb86ce96e43338b0b82002f2f190aa3f34
IbrahimBinYasir <ibrahimyasir6501@gmail.com>
2021-09-06 06:11:07 +0900

    About me

--- src/components/About.js
+++ src/components/About.js
@@ -10,12 +10,11 @@ export default function About(){
                     <h1 className = "title-font sm:text-4xl text-3xl mb-4 font-medium text white">
                         Hi, I'm Ibrahim
                         <br className = "hidden lg:inline-block" />
-                        About
+
                     </h1>
                     <p className = "mb-8 leading-relaxed">
-                        A Robotics graduate with undergrad in Mechanical Engineering. Experienced in concept generation and development from ideation to production of robotic technologies. Proficient in Mechanical Design (Solidworks), Control and Acquisition (MATLAB/LabView/ National Instruments), Prototyping (MEMS), Programming, and Project Planning/Management.
-My passion is in designing, researching practical technology for human-robot interaction in rehabilitative robotics. Self-motivated, honest, convivial, optimistic, and grit sums it up well. I am always looking to learn and contribute for the purpose betterment of quality of life . Robots being used for bridging the gap between what’s considered able and disabled drives my research interest. Currently working on innovative solutions to assist surgeons using a tactile sensor. In my free time, I like to travel, play soccer, and volunteer ^-^
-                    </p>
+                     A Robotics graduate with undergrad in Mechanical Engineering. Experienced in concept generation and development from ideation to
+                        production of robotic technologies. Proficient in Mechanical Design (Solidworks), Control and Acquisition (MATLAB/LabView), Prototyping (MEMS), Programming. My passion is in designing, researching practical technology for human-robot interaction in rehabilitative robotics. I am always looking to learn and contribute for the purpose betterment of quality of life. In my free time, I like to travel, play soccer, and volunteer ^-^                    </p>
                     <div className = "flex justify-center">
                         <a  href = "#contact"
                             className = "inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,9 +12,7 @@ export default function Skills(){
                         Skills &amp; Technologies
                     </h1>
                     <p className = "text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">{/* text size and line height */}
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-                        ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-                        possimus est.
+                        Stuff I have gotten my hands dirty with
                     </p>
                 </div>
                 <div className = "flex flex-wrap lg:w-4/5 sm:mx-auto sm: mb-2 -mx-2">{/* negative margin: pulls the element towards the previous element. Where margin pushes away, this pulls it closer to it*/}
