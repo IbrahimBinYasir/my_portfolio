6c7f37bcae0c2b9d41587ea7b4d7373d8218d7c7
IbrahimBinYasir <ibrahimyasir6501@gmail.com>
2021-09-15 00:26:12 +0900

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ export default function About(){
     return(
         <section id = "about">
             <div className = "container mx-auto flex px-10 py-20 md:flex-rpw flex-col items-center">
-                <div className = "lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start mb-16 md:mb-0 items-center text-center">
-                    <h1 className = "title-font sm:text-4xl text-3xl mb-4 font-medium text white">
+                <div className = "lg:flex-grow md:w lg:pr-24 md:pr-16 flex flex-col md:items-start mb-16 md:mb-0 items-center text-center">
+                    <h1 className = "title-font sm:text-4xl text-3xl mb-4 mx-auto font-medium text white">
                         Hi, I'm Ibrahim
                         <br className = "hidden lg:inline-block" />
                     </h1>
@@ -19,7 +19,8 @@ export default function About(){
                     </img>
 
                     <p className = "mb-8 leading-relaxed">
-                     A Robotics graduate with undergrad in Mechanical Engineering. Experienced in concept generation and development from ideation to
+
+                        A Robotics graduate with undergrad in Mechanical Engineering. Experienced in concept generation and development from ideation to
                         production of robotic technologies. Proficient in Mechanical Design (Solidworks), Control and Acquisition (MATLAB/LabView), Prototyping (MEMS), Programming. My passion is in designing, researching practical technology for human-robot interaction in rehabilitative robotics. I am always looking to learn and contribute for the purpose betterment of quality of life. In my free time, I like to travel, play soccer, and volunteer ^-^
                     </p>
                     </div>
