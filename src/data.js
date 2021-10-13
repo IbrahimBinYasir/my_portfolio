cd5ceeda624cf814e6899913255df3f83e785f90
IbrahimBinYasir <ibrahimyasir6501@gmail.com>
2021-10-13 22:51:45 +0900

    Hwayeong Testimonial

--- src/data.js
+++ src/data.js
@@ -56,6 +56,15 @@ export const testimonials = [
     name: "Muhammad Ahmad Qadri",
     company: "Director at Qadri Group",
   },
+      {
+    quote:
+      "The two words I could come up with about Ibrahim are passionate and patient. \n" +
+        "He and I did a project together in the lecture about the soft robot. He reached me first to do the project together and actively worked from start to finish. He was a creative thinker as well as a person who could look at each idea critically to reach the best idea, which I personally think is important for the engineer to develop something that really can help people. He was also a person who could patiently teach others what he is good at. As I was dealing with SMA (shape memory alloy) for the first time in the project, he calmly taught me how to deal with it and encouraged me to do it again even if I did something wrong. Not only did I learn a lot while working on the project with him, but also it was a fortune to get to know him as a fellow researcher and a good friend.",
+    image: "./Hwayeong.jpg",
+    name: "Hwayeong Jeong",
+    company: "PhD. KAIST",
+  },
+
   {
     quote:
       "I worked with Ibrahim during my Bachelor's degree for our final year project. We set an ambitious goal for ourselves, some may say over ambitious, but we spared no effort into trying to achieve it. Ibrahim was there every step of the way and people may find a number of faults with him but I don't think anyone can say he doesn't put in the time and effort. Lastly he is a fun person to work with which brightened up the mood when we found ourselves stuck and banging our heads against the wall.",
