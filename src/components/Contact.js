91b9f97394411d8293de138511ec0d051b0f2300
IbrahimBinYasir <ibrahimyasir6501@gmail.com>
2022-01-19 00:06:50 +0900

    c

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -73,9 +73,10 @@ export default function Contact(){
                 <form
                 name = "test"
                 method = "POST"
-                onSubmit = {handleSubmit}
-                className = "lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+                //onSubmit = {handleSubmit}
+                onSubmit = "submit"
                 data-netlify="true"
+                className = "lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
                 >
                     <input type="hidden" name="form-name" value="test"/>
                     <h2 className="text-black sm:text-4xl text-3xl mb-1 font-medium title-font">
